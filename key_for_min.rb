d34392d3b48f9053fb20e9069d77e76023109939
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-05 07:07:49 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_value = 0 
   small_key = nil
   
-  name_hash.collect do |key, value| 
+  name_hash.each do |key, value| 
     if min_value == 0 || value < min_value
       min_value = value 
       small_key = key
