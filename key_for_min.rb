c67631fcbae379cba664ed776a457084abac2e37
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-05 07:04:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  some_hash = {} 
   min_value = 0 
+  small_key = nil
   
   some_hash.collect do |key, value| 
     if min_value == 0 || value < min_value
       min_value = value 
-  end    
+      small_key = key
+  end   
+  small_key
 end
   end
